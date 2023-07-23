fabf340173a79cc5a05d84d32af1a1e012d8d320
bisquito <defiant20@gmail.com>
2023-07-23 11:21:45 +0300

    clear exer 12 for new try

--- exercises/12_useful_modules/task_12_3.py
+++ exercises/12_useful_modules/task_12_3.py
@@ -18,18 +18,3 @@ Reachable    Unreachable
              10.1.1.9
 
 """
-
-
-from tabulate import tabulate
-
-
-avaiable_ip_list = ['8.8.8.8', '127.0.12.12']
-unavailable_ip_list = ['115.115.115.115', '192.55.13.4']
-
-def print_ip_table(reach, unreach):
-    ips = {'Reachable': reach, 'Unreachable': unreach}
-    return(tabulate(ips, headers='keys'))
-
-
-if __name__ == '__main__':
-    print(print_ip_table(avaiable_ip_list, unavailable_ip_list))
